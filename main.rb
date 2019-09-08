1adb2e2748cf7562aba7e3a6c265fce9a278d7fb
Uzochukwu Obeki <uobeki.40@gmail.com>
2019-09-08 13:16:18 +0000

    Automatically backed up by Learn

---
+++ main.rb
@@ -0,0 +1,10 @@
+ # method name      first_argument, second_argument
+def greeting_programmer(name, language)
+  puts "Hello, #{name}. We heard you are a great #{language} programmer."
+end
+ 
+greeting_programmer("Sophie", "Ruby")
+# > Hello, Sophie. We heard you are a great Ruby programmer.
+ 
+greeting_programmer("Steven", "Elixir")
+# > Hello, Steven. We heard you are a great Elixir programmer.
